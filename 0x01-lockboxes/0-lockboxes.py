9b9169f351f1941c90d47a58d66a04d9f01b9f08
Josh-Techie <youse2002@outlook.fr>
2024-01-11 14:48:32 +0100

    0-lockboxes-finalCheck

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
 
+# Lockboxes
+# Contains method that finds the keys to open other lockboxes
+
 def canUnlockAll(boxes):
-    # Determines if all the boxes can be opened.
-    # :param boxes: List of lists representing locked boxes.
-    # :return: True if all boxes can be opened, else False.
-    if not boxes:
-        return False  # No boxes to open
-    # Initialize a set to track reachable boxes
-    reachable_boxes = {0}
-    # Iterate until no more changes in reachable boxes
-    while True:
-        # Keep track of the previous set of reachable boxes
-        prev_reachable_boxes = set(reachable_boxes)
-        # Iterate through keys in all reachable boxes
-        for box_id in prev_reachable_boxes.copy():
-            # copy for iteration
-            reachable_boxes.update(boxes[box_id])
-        # Check for changes in reachable boxes
-        if prev_reachable_boxes == reachable_boxes:
-            break
-    # Check if all boxes are reachable
-    return len(reachable_boxes) == len(boxes)
+    """
+    Function that determines if you can open all the lockboxes
+    Args:
+        boxes: list of lists of integers
+    Returns:
+        True if you can open all the lockboxes, False otherwise
+    """
+    unlocked_boxes = [0]
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            if key < len(boxes) and key not in unlocked_boxes \
+               and key != box_id:
+                unlocked_boxes.append(key)
+    if len(unlocked_boxes) == len(boxes):
+        return True
+    return False
