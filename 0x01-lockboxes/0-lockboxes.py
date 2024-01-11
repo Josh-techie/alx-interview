d12713cb4e3e84964a65e46a37c60f72838f1e3a
Josh-Techie <youse2002@outlook.fr>
2024-01-11 14:03:18 +0100

    ignore-testFile

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+def canUnlockAll(boxes):
+    # suppose box 0 is unlocked
+    unlocked = [0]
+    # take index of the box iterate and compare it with the key of the list in the big list
+    for box_id, box in enumerate(boxes):
+        if not box:
+            continue
+        for key in box:
+            # if matched continue iteration else return false
+            if key < len(boxes) and key not in unlocked and key != box_id:
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
