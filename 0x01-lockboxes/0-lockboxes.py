6fdb688178cfa74276de9864656f318f620d9174
Josh-Techie <youse2002@outlook.fr>
2024-01-11 14:26:50 +0100

    0-lockboxes-check

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,22 +1,9 @@
 #!/usr/bin/python3
 
-# def canUnlockAll(boxes):
-#     # suppose box 0 is unlocked
-#     unlocked = [0]
-#     # take index of the box iterate and compare it with the key of the list
-#     #  in the big list
-#     for box_id, box in enumerate(boxes):
-#         if not box:
-#             continue
-#         for key in box:
-#             # if matched continue iteration else return false
-#             if key < len(boxes) and key not in unlocked and key != box_id:
-#                 unlocked.append(key)
-#     if len(unlocked) == len(boxes):
-#         return True
-#     return False
-
 def canUnlockAll(boxes):
+    # Determines if all the boxes can be opened.
+    # :param boxes: List of lists representing locked boxes.
+    # :return: True if all boxes can be opened, else False.
     if not boxes:
         return False  # No boxes to open
     # Initialize a set to track reachable boxes
@@ -26,7 +13,8 @@ def canUnlockAll(boxes):
         # Keep track of the previous set of reachable boxes
         prev_reachable_boxes = set(reachable_boxes)
         # Iterate through keys in all reachable boxes
-        for box_id in prev_reachable_boxes.copy():  # copy for iteration
+        for box_id in prev_reachable_boxes.copy():
+            # copy for iteration
             reachable_boxes.update(boxes[box_id])
         # Check for changes in reachable boxes
         if prev_reachable_boxes == reachable_boxes:
